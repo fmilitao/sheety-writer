c6d4dd6ab581396f250d1626507641b29679e580
Filipe Militão <fm2097ad@gmail.com>
2019-07-28 19:51:17 +0100

    Test 1

---
+++ build/precache-manifest.80f72cd8cb8f62f8263920ae160b17b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a524b1090c96965f444d7fb5c3e8f05",
+    "url": "/sheety-writer/build/index.html"
+  },
+  {
+    "revision": "2a116080503a62110f6e",
+    "url": "/sheety-writer/build/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f6a0541b8604e7e98631",
+    "url": "/sheety-writer/build/static/js/2.39cedbdb.chunk.js"
+  },
+  {
+    "revision": "2a116080503a62110f6e",
+    "url": "/sheety-writer/build/static/js/main.fc635029.chunk.js"
+  },
+  {
+    "revision": "a9bc957e1ba2380807d1",
+    "url": "/sheety-writer/build/static/js/runtime~main.feaf9b1a.js"
+  }
+]);
